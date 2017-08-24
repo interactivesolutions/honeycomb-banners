1276626472c5745089239506b16cff9f37e19de3
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-24 16:04:18 +0300

    banner width fix

--- src/app/http/BannersHelper.php
+++ src/app/http/BannersHelper.php
@@ -124,7 +124,10 @@ class BannersHelper
      */
     public function imageTpl($src, $width, $height)
     {
-        return sprintf("<img src='%s' width='%s' height='%s'>", $src, $width, $height);
+        return "<div style='width: {$width}px; height: {$height}px; background: url($src); background-size: contain;background-repeat: no-repeat; max-width: 100%;'></div>";
+
+        /* @old */
+//        return sprintf("<img src='%s' width='%s' height='%s'>", $src, $width, $height);
     }
 
     /**
