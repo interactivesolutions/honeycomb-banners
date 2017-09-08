2c4101a394ac8031039d776dca90eff6b14d38bd
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-08 11:57:52 +0300

    added lt translations

---
+++ src/resources/lang/lt/banners.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'page_title'     => 'Baneriai',
+    'banner_type_id' => 'Banerio tipas',
+    'resource_id'    => 'Resuras',
+    'name'           => 'Pavadinimas',
+    'banner_url'     => 'Banerio nuoroda',
+    'start_at'       => 'Pradžia',
+    'end_at'         => 'Pabaiga',
+    'link_type'      => 'Nuorodos tipas',
+    'link_title'     => 'Nuorodos pavadinimas',
+    'hash'           => 'Kodas',
+    'type'           => 'Tipas',
+    'shows'          => 'Parodymai',
+    'clicks'         => 'Paspaudimai',
+    'sequence'       => 'Eiliškumas',
+
+    'errors' => [
+        'no_html_file'     => 'Html failas nerastas!',
+        'invalid_resource' => 'Netinka resursas! Prašome įkelti .jpg, .png arba .zip formato failus!',
+    ],
+];
---
+++ src/resources/lang/lt/banners_types.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'page_title' => 'Banerio tipai',
+    'name'       => 'Pavadinimas',
+    'width'      => 'Plotis',
+    'height'     => 'Aukštis',
+    'active'     => 'Aktyvus',
+];
